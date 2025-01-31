f055b7bac10d382875fa31896ec1fe261e31f09b
Sannikof <sannikovft@gmail.com>
2025-01-31 16:39:23 +0700

    Add files via upload

---
+++ JS-76/main.js
@@ -0,0 +1,8 @@
+const firstName = 'Костя'
+const lastName = 'Санников'
+const age = 20
+const height = 170
+const weight = 56
+
+console.log('let - изменчивая переменная, а const - постоянная, её не получится изменить' )
+console.log('Привет, меня зовут', firstName, lastName, 'мне', age, 'y.o,', 'мой рост', height, 'cm,', 'мой вес', weight, 'kg')
